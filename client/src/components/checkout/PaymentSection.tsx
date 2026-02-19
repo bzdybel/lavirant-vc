ced7ab1f84949707de1e6536657df4381618adbc
Lavirant Dev <dev@lavirant.pl>
2026-02-19 13:14:02 +0100

    feat: debug

--- client/src/components/checkout/PaymentSection.tsx
+++ client/src/components/checkout/PaymentSection.tsx
@@ -6,6 +6,7 @@ import content from "@/lib/content.json";
 const { mockMode } = content.checkout;
 
 export const PaymentSection = () => {
+  console.log(STRIPE_CONFIG.isMockMode)
    if (STRIPE_CONFIG.isMockMode === true) {
     return (
       <div className="p-4 bg-yellow-50 border border-yellow-200 rounded-lg">
